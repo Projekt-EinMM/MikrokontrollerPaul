6ce0b9dcc9a6065ff935b82140f0d0184d569675
unterwasserpanza <85389172+unterwasserpanza@users.noreply.github.com>
2022-07-12 13:50:27 +0200

    Update SoundController.ino

--- SoundController/SoundController.ino
+++ SoundController/SoundController.ino
@@ -1,27 +1,29 @@
-
+/*	Erster Prototyp vom Mikrokontroller 
+*	Hiermit wurde ein ESP-32 gesteuert
+*/ 
 void setup() {
-Serial.begin(9600); // setup serial
+Serial.begin(9600); 		//Starten des Mikrokontrollers
 
 }
-
-void loop() {
-
+/* Diese Methode wird durchgehend wiederholt */
+void loop() {			
 int value;
-value = analogRead(A0);
-
-
- if(value <= 20)
-  {
-    Serial.println("leise");
-
+value = analogRead(A0);		//Hier wird analoge Input gelesen, also das Mikrofon
+Serial.print(Value+" ")
+ if(value <= 20)			//Hier wird der Wert verarbeitet
+  {							//und es schreibt den jeweiligen Wert in den Outputstream zu Unity
+	Serial.write(0);
+    Serial.println("0");
   }
   else if( (value > 20) && ( value <= 45) )
   {
-    Serial.println("mittel-laut");
-  }
-  else if(value > 45)
+	Serial.write(1);
+    Serial.println("1");
+  } else if( (value > 45) )
   {
-    Serial.println("laut");
+	Serial.write(2);
+    Serial.println("2");
   }
+
 delay(50);
 }
